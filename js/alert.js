d847ea23c56a63f91aaa051810d269605665d889
Unknown <tabukashvili_irakli@yahoo.com>
2017-12-19 22:04:48 +0400

    mobile

--- js/alert.js
+++ js/alert.js
@@ -8,7 +8,11 @@ function myalert2()
 document.getElementById("alertin").style.display="block";
 }
 
-
+function menu()
+{
+document.getElementById("mobile_list").style.width="160px";
+event.cancelBubble = true;
+}
 
 
 // window click
@@ -16,4 +20,9 @@ window.onclick = function(event) {
     if (event.target == document.getElementById("id01")) {
         document.getElementById("id01").style.display = "none";
     }
+}
+function closemenu() {
+  if(document.getElementById("mobile_list").style.width == "160px" )
+  {
+  document.getElementById("mobile_list").style.width = "0";}
 }
